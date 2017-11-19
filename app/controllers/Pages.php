9b619ec9653d070d0509ff16b5758b74a60ad49c
Oliver Mensah <olivermensah96@gmail.com>
2017-11-19 11:07:26 +0000

    mapped methods and parameters to controllers

---
+++ app/controllers/Pages.php
@@ -0,0 +1,15 @@
+<?php 
+class Pages{
+    public function __construct(){
+       
+    }
+    public function index(){}
+        
+    public function get(){
+        echo "method";
+    }
+
+    public function getId($id){
+        echo "id ". $id ;
+    }
+}
